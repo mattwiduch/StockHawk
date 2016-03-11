1f7e724d11461e1430d6ed1b49bc8fbfc93dffd0
Mateusz Widuch <matt.widuch@gmail.com>
2016-03-11 20:57:07 +0000

    feat: Improve add stock dialog messages

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -198,6 +198,7 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
                 if (Utils.isNetworkAvailable(mContext)) {
                     new MaterialDialog.Builder(mContext).title(R.string.symbol_search)
                             .content(R.string.content_test)
+                            .positiveText(R.string.button_search)
                             .inputType(InputType.TYPE_CLASS_TEXT)
                             .inputRange(1, 5, Color.RED)
                             .input(R.string.input_hint, R.string.input_prefill, new MaterialDialog.InputCallback() {
