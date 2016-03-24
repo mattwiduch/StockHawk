baa8c557c3690ac41de4e4b5dbf1f2bc8832f13c
Mateusz Widuch <matt.widuch@gmail.com>
2016-03-24 16:51:30 +0000

    feat: Populate graph using stored stock data

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphFragment.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphFragment.java
@@ -32,8 +32,8 @@ import java.text.DecimalFormat;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class LineGraphFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
-    private static final int CURSOR_LOADER_ID = 0;
+public class LineGraphFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int CURSOR_LOADER_ID = 1;
     private Toolbar mToolbar;
     private LineChartView mChart;
     private String mStockSymbol;
@@ -95,16 +95,16 @@ public class LineGraphFragment extends Fragment implements LoaderManager.LoaderC
         return rootView;
     }
 
-    private void buildLineGraph(int isUp) {
+    private void buildLineGraph(float[] values, String[] labels, float minBid, float maxBid, int isUp) {
         int color = isUp == 1 ? getResources().getColor(R.color.green_high) : getResources().getColor(R.color.red_low);
         // Line chart customization
-        LineSet dataset = new LineSet(new String[] {"", "", "", "", "", "", "", "", ""}, mValues[1]);
+        LineSet dataset = new LineSet(labels, values);//new String[] {"", "", "", "", "", "", "", "", ""}, mValues[1]);
         dataset.setThickness(Tools.fromDpToPx(2.5f));
         dataset.setColor(Color.parseColor("#ffffff"));
-        dataset.setDotsRadius(Tools.fromDpToPx(4.5f));
-        dataset.setDotsColor(color);
-        dataset.setDotsStrokeColor(Color.parseColor("#ffffff"));
-        dataset.setDotsStrokeThickness(6f);
+//        dataset.setDotsRadius(Tools.fromDpToPx(4.5f));
+//        dataset.setDotsColor(color);
+//        dataset.setDotsStrokeColor(Color.parseColor("#ffffff"));
+//        dataset.setDotsStrokeThickness(6f);
         mChart.addData(dataset);
 
 // Generic chart customization
@@ -112,7 +112,7 @@ public class LineGraphFragment extends Fragment implements LoaderManager.LoaderC
         mChart.setYAxis(false);
         mChart.setBackgroundColor(color);
         int padding = getResources().getDimensionPixelSize(R.dimen.content_padding);
-        mChart.setPadding(padding, padding*3, padding, padding + padding/2);
+        mChart.setPadding(padding, padding * 3, padding, padding + padding / 2);
 
 
 // Paint object used to draw Grid
@@ -122,12 +122,35 @@ public class LineGraphFragment extends Fragment implements LoaderManager.LoaderC
         gridPaint.setAntiAlias(true);
         gridPaint.setStrokeWidth(Tools.fromDpToPx(1f));
         gridPaint.setPathEffect(new DashPathEffect(new float[]{8.0f, 8.0f}, 0));
-        mChart.setGrid(ChartView.GridType.HORIZONTAL, gridPaint);
+        mChart.setGrid(ChartView.GridType.HORIZONTAL, 10, 10, gridPaint);
 
 // Labels
         mChart.setXLabels(AxisController.LabelPosition.NONE);
         mChart.setYLabels(AxisController.LabelPosition.OUTSIDE);
-        mChart.setAxisBorderValues(0, 100, 50);
+
+        // Calculate min and max values to display on Y axis
+        int minValue, maxValue;
+        if (minBid - (int) minBid < 0.5) {
+            minValue = (int) minBid > 0 ? (int) minBid - 1 : 0;
+        } else {
+            minValue = (int) minBid;
+        }
+        if (maxBid - (int) maxBid < 0.5) {
+            maxValue = (int) maxBid + 1;
+        } else {
+            maxValue = (int) maxBid + 2;
+        }
+
+        // Set Y axis labels using custom step
+        int step = maxValue - minValue;
+        if (step == 1) {
+            mChart.setAxisBorderValues(minValue, maxValue);
+        } else if (step % 2 == 1) {
+            mChart.setAxisBorderValues(minValue, maxValue + 1, (step + 1) / 2);
+        } else {
+            mChart.setAxisBorderValues(minValue, maxValue, step / 2);
+        }
+
         mChart.setLabelsFormat(new DecimalFormat("#"));
         mChart.setLabelsColor(getResources().getColor(R.color.text_light_secondary));
         mChart.setFontSize(45);
@@ -138,7 +161,8 @@ public class LineGraphFragment extends Fragment implements LoaderManager.LoaderC
         anim.setEasing(new CircEase());
         anim.setOverlap(0.5f, new int[]{3, 2, 4, 1, 7, 5, 0, 6, 8});
         anim.setStartPoint(0.0f, 1.0f);
-        mChart.show(anim);
+        //mChart.show(anim);
+        mChart.show();
     }
 
     @Override
@@ -155,13 +179,14 @@ public class LineGraphFragment extends Fragment implements LoaderManager.LoaderC
     @Override
     public void onLoadFinished(android.support.v4.content.Loader<Cursor> loader, Cursor data) {
         if (data.moveToLast()) {
-            AppCompatActivity activity = (AppCompatActivity)getActivity();
+            AppCompatActivity activity = (AppCompatActivity) getActivity();
             if (mToolbar != null) {
                 activity.setSupportActionBar(mToolbar);
                 activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
                 activity.getSupportActionBar().setTitle(data.getString(data.getColumnIndex(QuoteColumns.NAME)));
             }
 
+            int isUp = data.getInt(data.getColumnIndex(QuoteColumns.IS_UP));
             stockSymbolTextview.setText(mStockSymbol);
             stockPriceTextview.setText(data.getString(data.getColumnIndex(QuoteColumns.BID_PRICE)));
             stockChangeTextview.setText(data.getString(data.getColumnIndex(QuoteColumns.PERCENT_CHANGE)));
@@ -176,22 +201,33 @@ public class LineGraphFragment extends Fragment implements LoaderManager.LoaderC
             stock1yTargetTextview.setText(getResources().getString(R.string.data_not_available));
             stockAvgVolumeTextview.setText(getResources().getString(R.string.data_not_available));
 
-            buildLineGraph(data.getInt(data.getColumnIndex(QuoteColumns.IS_UP)));
+            // Prepare data to be displayed on the graph
+            float[] stockValues = new float[data.getCount()];
+            String[] stockLabels = new String[data.getCount()];
+            float minBid = Float.MAX_VALUE;
+            float maxBid = Float.MIN_VALUE;
+
+            for (int position = 0; position < data.getCount(); position++) {
+                data.moveToPosition(position);
+                String bid = data.getString(data.getColumnIndex(QuoteColumns.BID_PRICE));
+                stockValues[position] = bid.equals(getString(R.string.data_not_available))
+                        ? 0f : Float.parseFloat(bid);
+                minBid = Math.min(minBid, stockValues[position]);
+                maxBid = Math.max(maxBid, stockValues[position]);
+                stockLabels[position] = "";
+            }
+
+            // Duplicate data point if there is only one so line always shows on the graph
+            if (data.getCount() == 1) {
+                stockValues = new float[]{stockValues[0], stockValues[0]};
+                stockLabels = new String[]{stockLabels[0], stockLabels[0]};
+            }
+
+            buildLineGraph(stockValues, stockLabels, minBid, maxBid, isUp);
         }
     }
 
     @Override
-    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {}
-
-//    /** Override up button behaviour so it navigates back to parent activity without recreating it. */
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                getActivity().onBackPressed();
-//                return (true);
-//        }
-//
-//        return (super.onOptionsItemSelected(item));
-//    }
+    public void onLoaderReset(android.support.v4.content.Loader<Cursor> loader) {
+    }
 }
